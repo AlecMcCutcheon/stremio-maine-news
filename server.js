55368e28cbf79c12fd1bed396d7031f7d4133a2b
Alec McCutcheon <alec@obc.tech>
2023-12-29 21:30:15 -0500

    updates

--- server.js
+++ server.js
@@ -3,3 +3,4 @@
 const { serveHTTP, publishToCentral } = require("stremio-addon-sdk")
 const addonInterface = require("./addon")
 serveHTTP(addonInterface, { port: process.env.PORT || 61327 })
+publishToCentral("https://a0da031547f5-stremio-maine-news.baby-beamup.club/manifest.json")
